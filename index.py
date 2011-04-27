000d39ba0e60801f0367ad5ede119a52dccbf4da
Lez <lez@cirka.hu>
2011-04-28 00:52:33 +0200

    index.html, templating

--- index.py
+++ index.py
@@ -4,12 +4,19 @@ from google.appengine.ext import db
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from django.utils import simplejson as json
+from mako.template import Template
 
 
 class Index(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('h')
 
+        
+        template = Template(filename="index.html",
+                            input_encoding='utf8',
+                            output_encoding='utf8')
+
+        output = template.render(kilometers=9999)
+        self.response.out.write(output)
 
 application = webapp.WSGIApplication(
                                      [('/', Index)],
